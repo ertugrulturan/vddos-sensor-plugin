ef0be13719e0456fcdae0f2bcc6996235c7818ba
Ertuğrul TURAN <60829297+ertugrulturan@users.noreply.github.com>
2021-02-19 20:09:18 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if ! [ $(id -u) = 0 ]; then
+   echo "only root run installer"
+   exit 1
+fi
+if [ ! -f /vddos/vddos ]; then
+	echo "Firt VDDOS INSTALL"
+fi
+cd /root/
+wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/bot.php
+wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/bot.sh
+wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/bot2.php
+wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/cookiess.sh
+wget https://github.com/ertugrulturan/vddos-sensor-plugin/blob/main/under.sh
+clear
+echo "Only bot.sh And under.sh Domain Name Edit And Run Script!"
+echo "Start (Run Command) : $ screen -S "t13r" bash /root/bot.sh"
