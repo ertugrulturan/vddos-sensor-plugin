14ef7e90bb537b03137418c3389644dc7e1ed00c
Ertuğrul TURAN <60829297+ertugrulturan@users.noreply.github.com>
2021-03-04 17:35:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ if [ ! -f /vddos/vddos ]; then
 	echo "First VDDOS INSTALL"
 fi
 yum install net-tools
+yum install screen
 cd /root/
 wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/bot.php
 wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/bot.sh
