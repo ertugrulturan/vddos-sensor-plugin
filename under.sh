61164ccf4c46c9499fc9ab13dc2bc172bff78b63
Ertuğrul TURAN <60829297+ertugrulturan@users.noreply.github.com>
2021-03-04 03:19:04 +0300

    Update under.sh

--- under.sh
+++ under.sh
@@ -2,7 +2,7 @@
 screen -X -S t13r kill
 vddos-switch domain1.com captcha
 vddos-switch domain2.com captcha
-bash /root/cookieuam.sh
+vddos reload
 while true
 do
 netstat -tn | awk '{print $5}' | sed -e 's/:.*//' | grep '\.'| sort | uniq -c | sort -nr | head -24 > logs.txt
