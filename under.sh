3f1b9ecda999e2e5f72293806de9d66e521cf776
Ertuğrul TURAN <60829297+ertugrulturan@users.noreply.github.com>
2021-03-14 04:47:31 +0300

    Update under.sh

--- under.sh
+++ under.sh
@@ -8,5 +8,5 @@ do
 netstat -tn | awk '{print $5}' | sed -e 's/:.*//' | grep '\.'| sort | uniq -c | sort -nr | head -24 > logs.txt
 cat logs.txt | base64 > antiddos.txt
 php /root/bot2.php
-sleep 1.5
+sleep 1.4
 done
