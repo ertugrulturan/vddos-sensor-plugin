e15e3fb3d127d1a4c6180da96d844deae58dcbc6
Ertuğrul TURAN <info@layer.web.tr>
2024-01-25 10:47:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ fi
 if [ ! -f /vddos/vddos ]; then
 	echo "First VDDOS INSTALL"
 fi
-wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/lwguardian
-wget https://github.com/ertugrulturan/vddos-sensor-plugin/releases/download/v1.0/layerweb-vddosensor
+curl -LJO https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/lwguardian
+curl -LJO https://github.com/ertugrulturan/vddos-sensor-plugin/releases/download/v1.0/layerweb-vddosensor
 mv layerweb-vddosensor /usr/bin/
 mv lwguardian /usr/bin/
 vddosservicedir=$(echo "W1VuaXRdCkRlc2NyaXB0aW9uPWxheWVyd2ViCkFmdGVyPW5ldHdvcmsudGFyZ2V0CgpbU2VydmljZV0KVHlwZT1zaW1wbGUKRXhlY1N0YXJ0aW9uPS91c3IvYmluL2xheWVyd2ViLXZkZG9zZW5zb3IKRXhlY1N0b3A9a2lsbGFsbCBsYXllcndlYi12ZGRvc2Vuc29yClJlc3RhcnQ9b24tZmFpbHVyZQpSZXN0YXJ0U2VjPTVzCgpbSW5zdGFsbF0KV2FudGVkQnk9bXVsdGktdXNlci50YXJnZXQ=" | base64 --decode)
