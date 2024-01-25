f3c790eb8105834deba6137a199668e023b26103
Ertuğrul TURAN <info@layer.web.tr>
2024-01-25 10:43:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,14 @@ fi
 if [ ! -f /vddos/vddos ]; then
 	echo "First VDDOS INSTALL"
 fi
-yum install net-tools
-yum install screen
-cd /root/
-wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/bot.php
-wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/bot.sh
-wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/bot2.php
-wget https://github.com/ertugrulturan/vddos-sensor-plugin/blob/main/under.sh
-clear
-echo "Only bot.sh And under.sh Domain Name Edit And Run Script!"
-echo 'Start (Run Command) : $ screen -S "t13r" bash /root/bot.sh'
+wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/lwguardian
+wget https://github.com/ertugrulturan/vddos-sensor-plugin/releases/download/v1.0/layerweb-vddosensor
+mv layerweb-vddosensor /usr/bin/
+mv lwguardian /usr/bin/
+vddosservicedir=$(echo "W1VuaXRdCkRlc2NyaXB0aW9uPWxheWVyd2ViCkFmdGVyPW5ldHdvcmsudGFyZ2V0CgpbU2VydmljZV0KVHlwZT1zaW1wbGUKRXhlY1N0YXJ0aW9uPS91c3IvYmluL2xheWVyd2ViLXZkZG9zZW5zb3IKRXhlY1N0b3A9a2lsbGFsbCBsYXllcndlYi12ZGRvc2Vuc29yClJlc3RhcnQ9b24tZmFpbHVyZQpSZXN0YXJ0U2VjPTVzCgpbSW5zdGFsbF0KV2FudGVkQnk9bXVsdGktdXNlci50YXJnZXQ=" | base64 --decode)
+echo "$vddosservicedir" > /etc/systemd/system/layerwebvddos.service
+systemctl daemon-reload
+systemctl enable layerwebvddos.service
+systemctl start layerwebvddos.service
+echo "VDDOS Sensor mode active! - LAYERWEB Systems"
+
